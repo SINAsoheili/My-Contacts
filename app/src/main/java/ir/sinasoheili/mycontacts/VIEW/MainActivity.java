03789d4bff2884806154b0defa0263efb6156000
unknown <sinasoheili79@gmail.com>
2020-07-08 17:25:15 +0430

    start project

---
+++ app/src/main/java/ir/sinasoheili/mycontacts/VIEW/MainActivity.java
@@ -0,0 +1,18 @@
+package ir.sinasoheili.mycontacts.VIEW;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ir.sinasoheili.mycontacts.R;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
